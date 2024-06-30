27633d5b39f547c37e2954b08d206f1910cdadfd
tyone <leungtingyat19@gmail.com>
2024-06-30 17:48:15 +0800

    adjust build js

--- build-website.js
+++ build-website.js
@@ -19,26 +19,6 @@ jsonFiles.forEach((file) => {
   }
 });
 
-//add a new folder named "html" in the out directory, then add all the HTML files just created to the out/html directory
-ncp.limit = 16;
-// Create the out/html directory if it doesn't exist
-const outDir = "./out/html";
-if (!fs.existsSync(outDir)) {
-  fs.mkdirSync(outDir, { recursive: true });
-}
-// Move all HTML files to the out/html directory
-const htmlFiles = fs.readdirSync("./out");
-htmlFiles.forEach((file) => {
-  if (file.endsWith(".html")) {
-    fs.renameSync(path.join
-    ("./out", file), path.join(outDir, file));
-    }
-}
-);
-console.log("🚀 Move HTML files done!");
-
-
-
 
 // After building all the HTML files, copy the CSS and JS files to the out directory
 // copy the whole assets folder to the out/assets directory
